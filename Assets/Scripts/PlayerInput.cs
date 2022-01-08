e1dc005b8ce58dc3633676c6e2ea450d4e59ff94
DESKTOP-C61PO9C\Randomality <brownron199@yahoo.com>
2022-01-07 21:36:29 -0500

    Player input changes sprite's facing direction.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.UI; 
 
 public class PlayerInput : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRigidBody2D;
+
+    [SerializeField] float moveSpeedModifier;
 
    
     void Start()
     {
-        
+         myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
 
 
@@ -25,5 +29,20 @@ public class PlayerInput : MonoBehaviour
         moveInput = value.Get<Vector2>();
 
     }
+
+    void Run(){
+        Vector2  playerVelocity = new Vector2 (moveInput.x * moveSpeedModifier, myRigidBody2D.velocity.y);
+        myRigidBody2D.velocity = playerVelocity;
+
+    }
+
+    void FlipSprite(){
+
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidBody2D.velocity.x) > Mathf.Epsilon;
+
+        if(playerHorizontalSpeed){
+
+         transform.localScale =  new Vector2(Mathf.Sign(myRigidBody2D.velocity.x), 1f);
+        }
+    }
 }
-     
